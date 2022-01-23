156b9ad148c39be58ce6562d7feb0507e84fb1cf
throuz <64568129+throuz@users.noreply.github.com>
2022-01-23 22:18:44 +0800

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+git branch -M main
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
